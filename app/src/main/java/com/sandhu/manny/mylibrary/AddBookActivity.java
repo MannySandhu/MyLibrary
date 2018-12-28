fb5e57aaf7af000932d2018220ed3360b6ebeacd
MSandhu <manz1992@gmail.com>
2018-12-28 21:27:44 +0000

    Parsed JSON data.

--- app/src/main/java/com/sandhu/manny/mylibrary/AddBookActivity.java
+++ app/src/main/java/com/sandhu/manny/mylibrary/AddBookActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -32,13 +33,16 @@ public class AddBookActivity extends AppCompatActivity {
         // wire up widgets
         Button addButton = (Button) findViewById(R.id.addButton);
         final EditText isbnEditText = (EditText) findViewById(R.id.isbnEditText);
-        final EditText detailsEditText = (EditText) findViewById(R.id.detailsEditText);
-        String isbnValue = "";
+        final TextView titleTextView = (TextView) findViewById(R.id.titleTextView);
+        final TextView authorTextView = (TextView) findViewById(R.id.authorTextView);
+        final TextView editionTextView = (TextView) findViewById(R.id.editionTextView);
+        final TextView publishedTextView = (TextView) findViewById(R.id.publishedTextView);
 
+        String isbnValue = "";
         // example steve jobs book
         final String stevejobsbook = "9781451648546";
-
         final RequestQueue requestQueue = Volley.newRequestQueue(this);
+
 
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,23 +52,61 @@ public class AddBookActivity extends AppCompatActivity {
                 String url = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + stevejobsbook;
 
                 //Do http request
-                JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.GET, url,
+                final JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.GET, url,
                         null, new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
 
-                        detailsEditText.setText("Response => "+response.toString());
-                        //findViewById(R.id.progressBar1).setVisibility(View.GONE);
+                        String[] result = jsonParser(response);
+                        titleTextView.setText(result[0]);
+                        authorTextView.setText(result[1]);
+                        editionTextView.setText(result[2]);
+                        publishedTextView.setText(result[3]);
+
+                        //findViewById(R.id.addButton).setVisibility(View.GONE);
                     }
                 }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-
+                        error.printStackTrace();
                     }
                 });
+
                 requestQueue.add(jsObjRequest);
             }
         });
-
     }
+
+    public String[] jsonParser(JSONObject response){
+        String[] result = new String[4]; // volume information holder
+
+        try {
+            JSONArray jsonArray = response.getJSONArray("items");
+            for(int i=0; i< jsonArray.length(); ++i){
+
+                JSONObject items = jsonArray.getJSONObject(i);
+
+                // get title info
+                String title = items.getJSONObject("volumeInfo").optString("title");
+                String subtitle = items.getJSONObject("volumeInfo").optString("subtitle");
+                result[0] = title + " : " + subtitle;
+
+                // get author info
+                result[1] = items.getJSONObject("volumeInfo").optString("authors");
+
+                // get category and page count info
+                String category = items.getJSONObject("volumeInfo").optString("categories");
+                String pageCount = items.getJSONObject("volumeInfo").optString("pageCount");
+                result[2] = category + " : " + pageCount + "p";
+
+                // get published date
+                String publishedDate = items.getJSONObject("volumeInfo").optString("publishedDate");
+                result[3] = publishedDate;
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
 }
