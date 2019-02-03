bec1e878c9924b765603cd4dc978bc8046b5ceeb
MSandhu <manz1992@gmail.com>
2019-02-03 21:23:39 +0000

    Added progress activity.

--- app/src/main/java/com/sandhu/manny/mylibrary/ProgressActivity.java
+++ app/src/main/java/com/sandhu/manny/mylibrary/ProgressActivity.java
@@ -13,16 +13,6 @@ public class ProgressActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 }
